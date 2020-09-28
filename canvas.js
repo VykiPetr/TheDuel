794652080e44e96612bbd2e3ecd803f4731dbd65
Vykintas Petrikas <vykintas.petrikas@gmail.com>
2020-09-28 11:47:55 +0300

    drawing status boxes, playing music on start but not always, random name select for the opponent, animations not finished on the losing screen, starting to make info screen, starting to make mechanincs, overall it looks messy.

--- canvas.js
+++ canvas.js
@@ -3,57 +3,109 @@ canvas.style.border = '1px solid black';
 let ctx = canvas.getContext('2d')
 // splashScreen
 let splash = new Image()
-splash.src = '/TheDuel/images/splashScreen.png'
+splash.src = '/images/splashScreen.png'
 // actionScreen
 let action = new Image()
-action.src = '/TheDuel/images/Action.png'
-action.addEventListener('load', () => {
-})
+action.src = '/images/Action.png'
+action.addEventListener('load', () => {})
 // loseScreen
 let loseBg = new Image()
-loseBg.src = '/TheDuel/images/loseBckgrnd.png'
+loseBg.src = '/images/loseBckgrnd.png'
 // player
 let playerImg = new Image()
-playerImg.src = '/TheDuel/images/PlayerIdle.png'
+playerImg.src = '/images/PlayerIdle.png'
+playerImg.addEventListener('load', () => {})    
 let playerX = 0
 let playerY = 0
+let playerStance = 0
+let playerHlth = 10
+let playerName = `Ser ${enteredName}`
 // opponent
+let opponentNameArr = ['Yeoman Yve','Sir Pers','Lord Joseph','Squire Fulk','Knight Eudon','Sir Barnaby','Sir Laselot','Squire Villiame']
 let opponentImg = new Image()
-opponentImg.src = '/TheDuel/images/OpponentIdle.png'
+opponentImg.src = '/images/OpponentIdle.png'
+let opponentX = 0
+let opponentY = 0
+let opponentStance = 0
+let opponentHlth = 5
+const randomName = () => {
+    let rng = Math.floor(Math.random()*opponentNameArr.length)
+    return opponentNameArr[rng]
+}
+let opponentName = randomName()
 
 const loseAnim = () => {
     playerY = 200
-    let intervalId = 0
     ctx.drawImage(loseBg, 0, 0)
-    intervalId = setInterval(() => {
-        ctx.drawImage(loseBg, 0, 0)
-        ctx.drawImage(playerImg, playerX, playerY)
-        if (playerImg.src === '/TheDuel/images/PlayerWalk1.png') {
-            playerImg.src = '/TheDuel/images/PlayerWalk2.png'
-        } else if (playerImg.src = '/TheDuel/images/PlayerWalk3.png') {
-            playerImg.src = '/TheDuel/images/PlayerWalk1.png'
-        } else {
-            playerImg.src = '/TheDuel/images/PlayerWalk3.png'
-        }
-        playerX += 5  
-    }, 100)
+    ctx.drawImage(playerImg, playerX, playerY)
+    playerImg.src = '/images/PlayerWalk1.png'
+    if (playerImg.src === '/images/PlayerWalk1.png') {
+        playerImg.src = '/images/PlayerWalk2.png'
+    } else if (playerImg.src === '/images/PlayerWalk3.png') {
+        playerImg.src = '/images/PlayerWalk1.png'
+    } else {
+        playerImg.src = '/images/PlayerWalk3.png'
+    }
+    playerX += 5
 }
 const clearCanvas = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 }
-
 const splashImg = () => {
     splash.addEventListener('load', () => {
         splashImg()
         ctx.drawImage(splash, 0, 0)
-        ctx.fillText('The Duel', 420, 200)
-        
+        ctx.font = '40px Verdana'
+        ctx.fillText('The Duel', 350, 170)
     })
 }
-
+const drawStatusBox = () => {
+    ctx.beginPath()
+    ctx.fillStyle = '#8f792c'
+    ctx.fillRect(40, 200, 150, 80)
+    ctx.fillStyle = 'black'
+    ctx.font = '20px Verdana'
+    ctx.fillText(playerName, 45, 220)
+    ctx.fillText(`Health: ${playerHlth}`, 45, 250)
+    ctx.closePath()
+    ctx.beginPath()
+    ctx.fillStyle = '#8f792c'
+    ctx.fillRect(700, 200, 150, 80)
+    ctx.fillStyle = 'black'
+    ctx.font = '20px Verdana'
+    ctx.fillText(opponentName, 710, 220)
+    ctx.fillText(`Health: ${opponentHlth}`, 710, 250)
+    ctx.closePath()
+}
 const actionImg = () => {
-        ctx.drawImage(action,0 ,0)
-        ctx.drawImage(playerImg, 50, 300)
-        ctx.drawImage(opponentImg, canvas.width-150, 300)
+    ctx.drawImage(action, 0, 0)
+    drawStatusBox()
+    ctx.drawImage(playerImg, 50, 300)
+    ctx.drawImage(opponentImg, canvas.width - 150, 300)
+    topBtn.addEventListener('click', () => {
+        playerImg.src = '/images/PlayerTop.png'
+        playerStance = 2
+        playerImg.addEventListener('load', () => {})
+        ctx.drawImage(action, 0, 0)
+        drawStatusBox()
+        ctx.drawImage(opponentImg, canvas.width - 150, 300)
+        ctx.drawImage(playerImg, 50, 260)
+    })
+    midBtn.addEventListener('click', () => {
+        playerImg.src = '/images/PlayerMid.png'
+        playerStance = 1
+        ctx.drawImage(action, 0, 0)
+        drawStatusBox()
+        ctx.drawImage(opponentImg, canvas.width - 150, 300)
+        ctx.drawImage(playerImg, 50, 280)
+    })
+    lowBtn.addEventListener('click', () => {
+        playerImg.src = '/images/PlayerBot.png'
+        playerStance = 0
+        ctx.drawImage(action, 0, 0)
+        drawStatusBox()
+        ctx.drawImage(opponentImg, canvas.width - 150, 300)
+        ctx.drawImage(playerImg, 50, 280)
+    })
 }
 splashImg()
--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 let startBtn = document.querySelector('.btn-primary')
+let nameBox = document.querySelector('.input-group.input-group-sm.mb-3')
+let posBtns = document.querySelector('.btn-group')
+let topBtn = document.querySelector('.top')
+let midBtn = document.querySelector('.mid')
+let lowBtn = document.querySelector('.low')
+let inputName = document.querySelector('.form-control')
+let clearIntervale = document.querySelector('.clearInterval')
+let enteredName = inputName.value
+
 let intervalId = 0
 startBtn.addEventListener('click', () => {
     startBtn.classList.add('hidden')
     actionImg()
 })
 
-document.addEventListener('load', () => {
-    splashImg()
-})
+
+// document.addEventListener('load', () => {
+//     splashImg()
+// })
 
 const startGame = () => {
+    inputName.addEventListener('change', (event) => {
+        console.log('event change')
+        inputName.value = event.target.value
+        enteredName = event.target.value
+    })
     startBtn.addEventListener('click', () => {
         startBtn.classList.add('hidden')
+        nameBox.classList.add('hidden')
+        posBtns.classList.remove('hidden')
         actionImg()
     })
 }
-intervalId - setInterval(() => {
+intervalId = setInterval(() => {
     requestAnimationFrame(startGame)
 },10)
+clearIntervale.addEventListener('click', () => {
+    clearInterval(intervalId)
+})
